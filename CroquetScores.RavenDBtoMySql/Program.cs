704a1de1243ef1613b47db997a6c521721357506
Tim Murphy <tim@26tp.com>
2023-12-08 07:49:21 +1100

    Add solution and console project

---
+++ CroquetScores.RavenDBtoMySql/Program.cs
@@ -0,0 +1,23 @@
+﻿try
+{
+    Console.WriteLine("Hello, World!");
+    throw new Exception("wtf");
+}
+catch (Exception exception)
+{
+    var foregroundColor = Console.ForegroundColor;
+
+    Console.ForegroundColor = ConsoleColor.Red;
+
+    Console.Error.WriteLine();
+    Console.Error.WriteLine("The following error occurred:");
+    Console.Error.WriteLine();
+    Console.Error.WriteLine(exception);
+    Console.Error.WriteLine();
+
+    Console.ForegroundColor = foregroundColor;
+
+    Environment.Exit(1);
+}
+
+
