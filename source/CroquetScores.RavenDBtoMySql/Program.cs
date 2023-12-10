631a0a71bd66d5ba07a3c57643e05760edfbf5a8
Tim Murphy <tim@26tp.com>
2023-12-11 07:51:17 +1100

    Refactor

--- source/CroquetScores.RavenDBtoMySql/Program.cs
+++ source/CroquetScores.RavenDBtoMySql/Program.cs
@@ -5,6 +5,7 @@ using System.Configuration;
 using System.Linq;
 using System.Web;
 using CroquetScores.RavenDB.Documents;
+using CroquetScores.RavenDBtoMySql.Support;
 
 namespace CroquetScores.RavenDBtoMySql
 {
@@ -15,7 +16,7 @@ namespace CroquetScores.RavenDBtoMySql
             try
             {
                 Console.WriteLine("Opening document store...");
-                using (var documentStore = GetDocumentStore())
+                using (var documentStore = RavenDbSupport.InitializeDocumentStore())
                 {
                     using (var session = documentStore.OpenSession())
                     {
@@ -42,39 +43,6 @@ namespace CroquetScores.RavenDBtoMySql
                 Environment.Exit(1);
             }
 
-        }
-        private static IDocumentStore GetDocumentStore()
-        {
-            var ravenDbConnectionString = GetRavenDbConnectionString();
-            var values = HttpUtility.ParseQueryString(ConvertConnectionStringToQueryString(ravenDbConnectionString));
-            var documentStore = new DocumentStore()
-            {
-                ApiKey = values["ApiKey"],
-                Url = values["Url"],
-                DefaultDatabase = values["Database"]
-            };
-
-            documentStore.Initialize();
-            return documentStore;
-        }
-
-        private static string GetRavenDbConnectionString()
-        {
-            var ravenDbConnectionString = ConfigurationManager.AppSettings["RavenDbConnectionString"];
-
-            if (string.IsNullOrWhiteSpace(ravenDbConnectionString))
-            {
-                throw new Exception("RavenDbConnectionString has not been set in AppSettings.config.");
-            }
-
-            ravenDbConnectionString = ConvertConnectionStringToQueryString(ravenDbConnectionString);
-
-            return ravenDbConnectionString;
-        }
-
-        private static string ConvertConnectionStringToQueryString(string connectionString)
-        {
-            return connectionString.Trim(System.Convert.ToChar(";")).Replace(";", "&");
         }
     }
 
---
+++ source/CroquetScores.RavenDBtoMySql/Support/RavenDbSupport.cs
@@ -0,0 +1,49 @@
+﻿using Raven.Client.Document;
+using Raven.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CroquetScores.RavenDBtoMySql.Support
+{
+    internal class RavenDbSupport
+    {
+        public static IDocumentStore InitializeDocumentStore()
+        {
+            var ravenDbConnectionString = GetRavenDbConnectionString();
+            var values = HttpUtility.ParseQueryString(ConvertConnectionStringToQueryString(ravenDbConnectionString));
+            var documentStore = new DocumentStore()
+            {
+                ApiKey = values["ApiKey"],
+                Url = values["Url"],
+                DefaultDatabase = values["Database"]
+            };
+
+            documentStore.Initialize();
+            return documentStore;
+        }
+
+        private static string GetRavenDbConnectionString()
+        {
+            var ravenDbConnectionString = ConfigurationManager.AppSettings["RavenDbConnectionString"];
+
+            if (string.IsNullOrWhiteSpace(ravenDbConnectionString))
+            {
+                throw new Exception("RavenDbConnectionString has not been set in AppSettings.config.");
+            }
+
+            ravenDbConnectionString = ConvertConnectionStringToQueryString(ravenDbConnectionString);
+
+            return ravenDbConnectionString;
+        }
+
+        private static string ConvertConnectionStringToQueryString(string connectionString)
+        {
+            return connectionString.Trim(System.Convert.ToChar(";")).Replace(";", "&");
+        }
+    }
+}
